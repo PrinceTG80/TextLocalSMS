5822fd104e04059b521b67b32376bdbb9f328589
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-17 12:07:58 +0530

    random OTP generated

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 from flask import request
 import urllib.request
 import urllib.parse
+import math, random
 
 app = flask.Flask(__name__)
 def sendSMS(apikey, numbers, sender, message):
@@ -10,11 +11,19 @@ def sendSMS(apikey, numbers, sender, message):
         + urllib.parse.urlencode(params))
     return (f.read(), f.code)
 
+def generateOTP():
+  digits = "0123456789"
+  OTP = ""
+  print("OTP Generation")
+  for i in range(6):
+        OTP += digits[math.floor(random.random() * 10)]
+  return OTP
+
 @app.route('/', methods = ['GET'])
 def home():
     apikey = str(request.args['token'])
     numbers = str(request.args['mobile'])
-    otpCode = str(request.args['otp'])
+    otpCode = str(generateOTP())
     clientName = str(request.args['Name'])
     try: 
         resp, code = sendSMS(apikey, numbers,'USMRTA', 'Dear '+clientName+', OTP is '+otpCode+' for Gate Pass, kindly enter it to confirm your gate pass. thank you! Managed By U SMART AI LAB.')
